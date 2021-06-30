495fdda86312150ad8b1fe8d453f72f75f7e8a8b
Kareem Yomi <kareemyomi91@gmail.com>
2021-06-30 13:39:49 +0100

    deploy

--- admin/pages/config/config.php
+++ admin/pages/config/config.php
@@ -1,8 +1,15 @@
 <?php
-$host="localhost"; //hostname
-$username="root"; //mysql username
-$password="6969"; //mysql password
-$db_name="sporylzm_porlt"; //Database name
+# Localhost
+// $host="localhost"; //hostname
+// $username="root"; //mysql username
+// $password="6969"; //mysql password
+// $db_name="sporylzm_porlt"; //Database name
+
+  // Heroku
+  $host = "us-cdbr-east-04.cleardb.com";
+  $db_name = "heroku_f4565c2e7db9247";
+  $username = "be2ec2be0cee8b";
+  $password = "2f4ac3b3";
 //connect to database
 $con=mysqli_connect($host,$username,$password, $db_name);
 if(!$con)
--- core/config.php
+++ core/config.php
@@ -1,12 +1,19 @@
 <?php
-$host="localhost"; //hostname
-$username="root"; //mysql username
-$password="6969"; //mysql password
-$db_name="sporylzm_porlt"; //Database name
+# Localhost
+// $host="localhost"; //hostname
+// $username="root"; //mysql username
+// $password="6969"; //mysql password
+// $db_name="sporylzm_porlt"; //Database name
+
+// Heroku
+$host = "us-cdbr-east-04.cleardb.com";
+$db_name = "heroku_f4565c2e7db9247";
+$username = "be2ec2be0cee8b";
+$password = "2f4ac3b3";
 //connect to database
 $con=mysqli_connect($host,$username,$password, $db_name);
 if(!$con)
-{die('could not connect1:'.mysqli_error());}
+{die('could not connect1 Error:'.mysqli_error());}
 //mysql_select_db($db_name,$con)
 //or die("could not connect2: ".mysql_error());
 ?>
